de04e988fa2e837430cebbac5df1a88aa15b53ec
YesCoach <sbsii1020@gmail.com>
2023-07-26 18:13:27 +0900

    feat: LoginViewController 텍스트필드 및 화면전환 구현 - EmailTextField, PasswordTextField가 비어있으면 얼럿 호출 - Login 버튼을 누르고 텍스트필드 체크가 통과하면 MovieViewController present - 둘러보기 버튼 누르면 세그로 화면 전환(modal, fullScreen)

--- jackflix/LoginViewController.swift
+++ jackflix/LoginViewController.swift
@@ -19,9 +19,27 @@ final class LoginViewController: UIViewController {
         configureUI()
     }
 
-    // Any
-    @IBAction func keyboardDismiss(_ sender: Any) {
+    @IBAction func dismissKeyboard(_ sender: Any) {
         view.endEditing(true)
+    }
+
+    @IBAction func didKeyboardReturnEntered(_ sender: UITextField) {
+
+    }
+
+    @IBAction func didEmailLoginButtonTouched(_ sender: UIButton) {
+        guard !emailTextField.text!.isEmpty else {
+            presentAlert(message: "이메일을 입력해주세요")
+            return
+        }
+        guard !passwordTextField.text!.isEmpty else {
+            presentAlert(message: "비밀번호를 입력해주세요")
+            return
+        }
+        let movieViewController = UIStoryboard(name: "Main", bundle: nil)
+            .instantiateViewController(withIdentifier: "MovieViewController")
+        movieViewController.modalPresentationStyle = .fullScreen
+        present(movieViewController, animated: true)
     }
 }
 
@@ -63,5 +81,22 @@ private extension LoginViewController {
 
         emailTextField.placeholder = "이메일을 입력하세요"
         passwordTextField.placeholder = "비밀번호를 입력하세요"
+    }
+
+    func presentAlert(
+        title: String = "",
+        message: String = "",
+        preferredStyle: UIAlertController.Style = .alert
+    ) {
+        let alert = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: preferredStyle
+        )
+
+        let action = UIAlertAction(title: "확인", style: .default)
+        alert.addAction(action)
+
+        present(alert, animated: true)
     }
 }
